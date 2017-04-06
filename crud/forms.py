4cc6ed79bc2cb81359560b91f5b87a4d70449378
jarecky <j.swierek@itm.biz.pl>
2017-04-06 11:01:49 +0200

    CRISPY_TEMPLATE_PACK + FormActions

--- crud/forms.py
+++ crud/forms.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from django import forms
+from django.urls import reverse
 
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+from crispy_forms.layout import Layout, Fieldset, Submit
+from crispy_forms.bootstrap import FormActions
 
 
 class CrudForm(forms.Form):
@@ -20,6 +22,10 @@ class CrudForm(forms.Form):
     )
 
     def __init__(self, *args, **kwargs):
+
+        cancel_url = reverse('main_menu')
+        cancel = 'window.location.href="{}"'.format(cancel_url)
+
         super(CrudForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
@@ -28,7 +34,8 @@ class CrudForm(forms.Form):
                 'model_name',
                 'views'
             ),
-            ButtonHolder(
-                Submit('submit', 'Submit', css_class='button white')
+            FormActions(
+                Submit('save', 'Create', css_class="btn-primary"),
+                Submit('cancel', 'Cancel', onclick=cancel)
             )
         )
